5903b28f7b0c2441ff2ddbb12db63ce6104308ee
marharyta-ilinova <marharyta.ilinova@amo.tech>
2026-03-14 21:18:02 +0200

    some refactoring

---
+++ src/test/java/io/testomat/e2e_tests_light/BaseTest.java
@@ -0,0 +1,25 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package io.testomat.e2e_tests_light;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class BaseTest {
+    public static Dotenv dotenv = Dotenv.load();
+
+    public BaseTest() {
+    }
+
+    public Integer parseInteger(String text) {
+        String digitText = text.replaceAll("\\D+", "");
+        return Integer.parseInt(digitText);
+    }
+
+    public Double parseDouble(String text) {
+        String doubleText = text.replaceAll("\\d.", "");
+        return Double.parseDouble(doubleText);
+    }
+}
--- src/test/java/io/testomat/e2e_tests_light/ProjectPageTests.java
+++ src/test/java/io/testomat/e2e_tests_light/ProjectPageTests.java
@@ -1,33 +1,74 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package io.testomat.e2e_tests_light;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebElementCondition;
+import com.codeborne.selenide.WebElementsCondition;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+public class ProjectPageTests extends BaseTest {
+    String login;
+    String password;
+    String baseUrl;
+    String targetProjectName;
 
-public class ProjectPageTests {
-    private static final Dotenv dotenv = Dotenv.load();
+    public ProjectPageTests() {
+        this.login = dotenv.get("TEST_LOGIN");
+        this.password = dotenv.get("TEST_PASSWORD");
+        this.baseUrl = dotenv.get("BASE_URL");
+        this.targetProjectName = "Testiks";
+    }
+
+    private static void waitForProjectIsLoaded(String projectName) {
+        Selenide.$(".sticky-header h2").shouldHave(new WebElementCondition[]{Condition.text(projectName)});
+    }
+
+    private static void selectProjectByName(String projectName) {
+        Selenide.$(Selectors.byText(projectName)).click();
+    }
+
+    private static void searchProject(String projectName) {
+        Selenide.$("#search").setValue(projectName);
+    }
+
+    private static void loginUser(String login, String password) {
+        Selenide.$("#content-desktop #user_email").setValue(login);
+        Selenide.$("#content-desktop #user_password").setValue(password);
+        Selenide.$("#content-desktop #user_remember_me").click();
+        Selenide.$("#content-desktop [name=\"commit\"]").click();
+        Selenide.$(".common-flash-success").shouldBe(new WebElementCondition[]{Condition.visible});
+    }
 
     @Test
     public void firstTest() {
+        Selenide.open(this.baseUrl);
+        loginUser(this.login, this.password);
+        searchProject(this.targetProjectName);
+        selectProjectByName(this.targetProjectName);
+        waitForProjectIsLoaded(this.targetProjectName);
+    }
 
-        //login
-        open("https://app.testomat.io/users/sign_in");
-        $("#content-desktop #user_email").setValue(dotenv.get("TEST_LOGIN"));
-        $("#content-desktop #user_password").setValue(dotenv.get("TEST_PASSWORD"));
-        $("#content-desktop #user_remember_me").click();
-        $("#content-desktop [name=\"commit\"]").click();
-        //check that login is successful
-        $(".common-flash-success").shouldBe(visible);
+    @Test
+    public void anotherTest() {
+        Selenide.open(this.baseUrl);
+        loginUser(this.login, this.password);
+        searchProject(this.targetProjectName);
+        ((ElementsCollection)Selenide.$$("#grid ul li").filter(Condition.visible)).shouldHave(new WebElementsCondition[]{CollectionCondition.size(1)});
+        Selenide.$("[title='Testiks'] p").shouldHave(new WebElementCondition[]{Condition.exactText("0 tests")});
+    }
 
-        //search and pick the project
-        $("#search").setValue(dotenv.get("TEST_PROJECT_NAME"));
-        $(byText(dotenv.get("TEST_PROJECT_NAME"))).click();
-        sleep(10000);
-        //check that project is opened
-        $(".sticky-header h2").shouldHave(text(dotenv.get("TEST_PROJECT_NAME")));
+    @Test
+    public void anotherTestCheck() {
+        Double digit = this.parseDouble("875767 tests");
+        System.out.println(digit);
+        System.out.println(digit instanceof Double);
     }
 }
