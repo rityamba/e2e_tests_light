35b7d7aec3cd5893ad625af564eeadc08bece46d
marharyta-ilinova <marharyta.ilinova@amo.tech>
2026-03-14 22:25:58 +0200

    some refactoring (done 2.0))

--- src/test/java/io/testomat/e2e_tests_light/ProjectPageTests.java
+++ src/test/java/io/testomat/e2e_tests_light/ProjectPageTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.size;
@@ -13,15 +14,19 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.e2e_tests_light.utils.StringParsers.parseInteger;
 
 public class ProjectPageTests extends BaseTest {
-    String login = dotenv.get("TEST_LOGIN");
-    String password = dotenv.get("TEST_PASSWORD");
-    String baseUrl = dotenv.get("BASE_URL");
+    static String login = dotenv.get("TEST_LOGIN");
+    static String password = dotenv.get("TEST_PASSWORD");
+    static String baseUrl = dotenv.get("BASE_URL");
     String targetProjectName = "Testiks";
+
+    @BeforeAll
+    static void openTestomatAndLogin(){
+        open(baseUrl);
+        loginUser(login, password);
+    }
 
     @Test
     public void firstTest() {
-        open(baseUrl);
-        loginUser(login, password);
         searchProject(targetProjectName);
         selectProjectByName(targetProjectName);
         waitForProjectIsLoaded(targetProjectName);
@@ -29,8 +34,6 @@ public class ProjectPageTests extends BaseTest {
 
     @Test
     public void anotherTest() {
-        open(baseUrl);
-        loginUser(login, password);
         searchProject(targetProjectName);
         SelenideElement targetProject = countOfProjectsShouldBeEqualTo(1).first();
         countOfTestsShouldBeEqualTo(targetProject, 0);
